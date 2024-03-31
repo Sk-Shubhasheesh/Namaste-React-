75b939cd4d14fcaa7dbe067340289acd471bc6b6
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-03-31 16:31:25 +0530

    Episode 3 - part 1 & 2 Done

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "This is Namaste React 🚀"),
-      React.createElement("h2", {}, "I'm h2 tag"),
-    ]),
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h3", {}, "I'm h3 tag"),
-      React.createElement("h4", {}, "I'm h4 tag"),
-    ])
-]);
+
+// React Element => Object => HTMLElement(render )
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React🚀");
+
+// creating root
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// render
+root.render(heading);
