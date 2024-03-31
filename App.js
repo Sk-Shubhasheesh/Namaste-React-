63e5d9cdc62dbc3584cb72df1cdb8785ccb29e37
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-03-31 21:07:44 +0530

    Episod 03 - Completed

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const HeadingComponent = () => (
-  <div id="container">
-    <h1 className="heading">Namste React Functional Component</h1>
-  </div>
-);
-const title = (
+const Title = () => (
     <h1 className="head" tabIndex="5">
         Namaste React using JSX 🚀
-        <HeadingComponent/>
     </h1>
 );
 
-// //In JSX We can inject any javascript code inside {}
-// const HeadingComponent = () => (
-//   <div id="container">
-//     {title}
-//     <h1 className="heading">Namste React Functional Component</h1>
-//   </div>
-// );
+const HeadingComponent = () => (
+ <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1 className="heading">Namste React Functional Component</h1>   
+  </div>
+ );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+root.render(<HeadingComponent />);
 
-// rendering React Functional Component
-// root.render(<HeadingComponent/>);
-root.render(title);
