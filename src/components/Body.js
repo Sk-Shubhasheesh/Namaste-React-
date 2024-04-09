48fcdc50d1bdf6e33cdb44ba1952518d5a526dd5
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-04-09 20:28:25 +0530

    Episod 06 - part 6 done

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,54 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-
 const Body = () => {
   const [ListOfRestaurants, setListOfRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = async ()=> {
+  const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.87560&lng=80.91150&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [])
-  }
-  return ListOfRestaurants.length === 0 ? <Shimmer /> : (
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || []
+    );
+  };
+
+  return ListOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // implement filter the Restaurant card and update the Ui
+              // searchText
+              console.log(searchText);
+              const filterRestaurent = ListOfRestaurants.filter((resList) =>
+                resList.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setListOfRestaurant(filterRestaurent);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
