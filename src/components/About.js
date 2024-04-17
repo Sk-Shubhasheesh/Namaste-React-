a43e2f67a2f5257ff386373eeda86e6abdbffc33
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-04-17 14:55:11 +0530

    Episod 08 - Part 3 Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,14 @@
 import User from "./User";
 import UserClass from "./UserClass";
-const About = ()=> {
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-            <User name={"Shubhasheesh Kundu (function)"} />
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>This is Namaste React Web Series</h2>
 
-            <UserClass name={"Shubhasheesh Kundu (class)"} />
-        </div>
-    );
+      <UserClass name={"Shubhasheesh Kundu (class)"} />
+    </div>
+  );
 };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -9,11 +9,16 @@ class UserClass extends React.Component {
     }
   render() {
     // desturcuting
-    const {count2} = this.state
+    const {count} = this.state
     return (
       <div className="user-card">
-        <h1>Count: {this.state.count}</h1>
-        <h1>Count2: {count2}</h1>
+        <h1>Count: {count}</h1>
+        <button onClick={()=>{
+           // Never update State variable directly
+            this.setState({
+                count: this.state.count+1,
+            })
+        }}>Count Increase</button>
         <h2>Name: {this.props.name}</h2>
         <h3>Location: Jaunpur</h3>
         <h4>Contact: shubhasheeshkundu@gmail.com</h4>
